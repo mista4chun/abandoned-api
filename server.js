ffc1c307fb219ae276a4f65d16ec9d7a67fee5f9
Fortune Samson <mr4chun4u@gmail.com>
2024-01-27 13:18:49 +0100

    Third commit

--- server.js
+++ server.js
@@ -1,29 +1,10 @@
 const express = require("express");
 const port = 5000;
 const app = express();
-const ideas = [
-  {
-    id: 1,
-    text: "Positive NewsLetter, a newsletter that only shares positive, uplifting news",
-    tag: "Technology",
-    username: "TonyStark",
-    date: "2022-01-02",
-  },
-  {
-    id: 2,
-    text: "Milk cartons that turn a different color the older that your milk is getting",
-    tag: "Inventions",
-    username: "SteveRogers",
-    date: "2022-01-02",
-  },
-  {
-    id: 3,
-    text: "ATM location app which lets you know where the closest ATM is and if it is in service",
-    tag: "Software",
-    username: "BruceBanner",
-    date: "2022-01-02",
-  },
-];
+
+// Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the RandomIdeas API" });
